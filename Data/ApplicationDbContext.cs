607715ad5f3ddbc3123a009458bb9ec3d813b16e
IUnoMasI <jonathanddiaz368@gmail.com>
2024-03-20 18:35:24 -0400

    Initial commit of LabWebApp

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebLabApp.Models;
+
+namespace LabWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
